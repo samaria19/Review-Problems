fd6bb1c588ebe539d4ad1bc57ba58013b07d2886
Jonathan Corley <jcorley@noscihigh.org>
2021-03-15 16:10:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+
+///Strings
+//First, declare variable named firstName.
+
+//Assign firstName to a string value of your first name.
+
+//Declare another variable named lastName and assign it the value of your last name.
+
+//Then declare another variable named fullName and assign it to the current value of firstName and lastName and have the joined values separated with a space character.
+
+//Remember that computer languages will not add the space character for you so how would you do that manually?
+
+
+
+
+
+
+
+
+//Numbers
+//Declare a variable named luckyNumber.
+
+//Assign luckyNumber to a number of your choice.
+
+//Declare another variable named luckyNumberTimes2 and assign it the value of luckyNumber multiplied by 2.
+
+
+
+
+
+
+
+
+
+
+
+//Booleans
+//Declare a variable named myBool and assign it the value of true.
+
+
+//Then, declare another named isEqualTo and assign it the value of strictly comparing luckyNumber and luckyNumberTimes2.
+
+
+
+
+
+/* 
+1. Create a function factorial that when given a 
+number, returns the factorial of it. The factorial
+ of a number is the product of an integer and the 
+ integers below it.
+
+
+What are factorials? 
+https://en.wikipedia.org/wiki/Factorial
+
+For example:
+factorial(1); // => 1     (1)
+factorial(2); // => 2     (2 * 1)
+factorial(3); // => 6     (3 * 2 * 1)
+factorial(4); // => 24    (4 * 3 * 2 * 1)
+factorial(8); // => 40320 (8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
+
+
+
+  */
+
+function factorial(number){
+//CODE BELOW HERE
+
+
+
+//CODE ABOVE HERE
+}
+
+
+
+
+
+
+/*
+2. Create a function reverseString that takes in a string
+ and returns the reversed version.
+
+Example: 
+reverseString('Hello, World!'); // => '!dlroW ,olleH'
+
+*/
+
+function reverseString(string){
+  //CODE BELOW HERE
+
+
+
+  //CODE ABOVE HERE
+}
+
+
+
+
+
+/*
+3. Create a function that takes an input array and 
+add all numbers in the array.
+
+Example: 
+var myArray = [1,2,3,4,5]
+
+arraySum(myArray) => 15
+*/
+function arraySum(array){
+   
+    
+}
